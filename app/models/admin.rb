e62c119f77cdc4f66c60396b59d258d7715e28c0
rpuscas <rpuscas94@gmail.com>
2015-07-20 13:33:55 +0200

    Implemented login and customized login form

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,9 @@
 class Admin < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable and :omniauthable, :recoverable,
+  devise :database_authenticatable,:rememberable, :trackable
+
+ 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :admins
-  root 'tanks#index'
+
+  devise_scope :admin do
+    authenticated :admin do
+      root 'tanks#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   get 'order_by_title' => 'tanks#order_by_title'
   get 'order_by_id' =>    'tanks#order_by_id'
   resources :tanks do
