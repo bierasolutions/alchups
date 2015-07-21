9c389adede283db2fd15e424e06d1c682e51687e
Dani Latorre <dani@danilat.com>
2015-07-21 18:33:33 +0200

    added public page to show all the data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-
+    @tanks = Tank.all
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
   describe "GET 'index'" do
-    
+    before(:each) do
+      FactoryGirl.create(:tank)
+      FactoryGirl.create(:tank)
+      get :index
+    end
+    it "returns http success" do
+      expect(response).to be_success
+    end
+
+    it "has a list of tanks" do
+      expect(assigns(:tanks).size).to be 2
+    end
   end
 end
