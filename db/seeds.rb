5a2f959e792fc9c1a09ca9760914ea51e1c7fe9b
rpuscas <rpuscas94@gmail.com>
2015-07-25 18:18:45 +0200

    convert utm coordinates to latitude-longitude before showing tanks in the map

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Tank.create(title: "ALCHUP DE LA TORRE BASO", description: "Una descripcion", latitude: 41.8900475, longitude: 0.31794049999999996, notes: "TRANSFORMADO EN PISCINA AZUL")
+Tank.create(title: "ALCHUP DE LA TORRE BASO", description: "Una descripcion", latitude: 4642975.716565, longitude: 775285.957988, notes: "TRANSFORMADO EN PISCINA AZUL")
 
 30.times do |n|
   title  = Faker::Lorem.word.upcase
   description = Faker::Lorem.paragraph
-  latitude = Faker::Number.decimal(2)
-  longitude = Faker::Number.decimal(2)
+  latitude = Faker::Number.between(4641000, 4649000)
+  longitude = Faker::Number.between(772000, 779000)
   notes = Faker::Lorem.sentence
   Tank.create( title:  title,
                description: description,
