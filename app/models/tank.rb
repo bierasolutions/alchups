75516026abc5401349e720cbebd2210220866751
rpuscas <rpuscas94@gmail.com>
2015-07-25 21:48:25 +0200

    implemented coordinates method in the model

--- app/models/tank.rb
+++ app/models/tank.rb
@@ -1,4 +1,8 @@
 class Tank < ActiveRecord::Base
   validates :title, presence:true
   has_many :photos
+
+  def coordinates
+  	Coordinates.utm_to_lat_long("WGS-84", self.latitude, self.longitude, "30N")
+  end
 end
