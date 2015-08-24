a2f67eca7333a7bf79f33bed6485d4eb3b5827df
Dani Latorre <dani@danilat.com>
2015-08-24 20:51:13 +0200

    two sample routes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,20 +9,48 @@ $(document).ready(function() {
 });
 	
 	function homeMap(){
-		var options = mapOptions(41.89, 0.3179,11);
+		var options = mapOptions(41.89, 0.3179,12);
 		var map = makeMap(options, 'map-home');
 
 		$('#alchups li').each(function() {
-	    	var lati = $(this).data("latitude");
-	    	var longi = $(this).data("longitude");
-	    	var contentInfo = '';
-    		var marker = makeMarker(lati,longi,map);
-	    	google.maps.event.addListener(marker, 'click', function() {
-	    			infowindow.setContent(contentInfo)
-       		  infowindow.open(map,marker);
-   			});
-    	
- 		 });	  
+    	var lati = $(this).data("latitude");
+    	var longi = $(this).data("longitude");
+    	var contentInfo = '';
+  		var marker = makeMarker(lati,longi,map);
+    	google.maps.event.addListener(marker, 'click', function() {
+    			infowindow.setContent(contentInfo)
+     		  infowindow.open(map,marker);
+ 			});
+ 		});	 
+
+	 	var route1Coordinates = [
+	    {lat: 41.93, lng: 0.31},
+	    {lat: 41.91, lng: 0.27},
+	    {lat: 41.90, lng: 0.32},
+	    {lat: 41.94, lng: 0.29}
+	  ];
+	  var route2Coordinates = [
+	    {lat: 41.88, lng: 0.39},
+	    {lat: 41.94, lng: 0.35},
+	    {lat: 41.90, lng: 0.27},
+	    {lat: 41.92, lng: 0.29}
+	  ];
+	  var route1Path = new google.maps.Polyline({
+	    path: route1Coordinates,
+	    geodesic: true,
+	    strokeColor: '#FFF',
+	    strokeOpacity: 0.7,
+	    strokeWeight: 5
+	  });
+	  var route2Path = new google.maps.Polyline({
+	    path: route2Coordinates,
+	    geodesic: true,
+	    strokeColor: '#FF0000',
+	    strokeOpacity: 0.7,
+	    strokeWeight: 5
+	  });
+	  route1Path.setMap(map); 
+	  route2Path.setMap(map); 
 	}
 	
 	function initializeIndex() {
