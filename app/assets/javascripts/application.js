c8347ebca5e8a176aa8ff347d7e0175ac5ac83fc
Dani Latorre <dani@danilat.com>
2015-09-25 20:35:21 +0200

    minor js change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,22 @@
 //= require_tree .
 
 $(document).ready(function() {
+  $.cookieBar({
+    message: 'Usamos cookies para poder tener estadísticas de uso de la web. Si continúas navegando estás dando tu consentimiento para la instalación de las cookies.',
+    acceptText: 'Acepto',
+    autoEnable: false,
+    acceptOnContinue: false,
+    acceptOnScroll: true,
+    acceptAnyClick: true
+  });
+  if(jQuery.cookieBar('cookies')){
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-918753-14', 'auto');
+    ga('send', 'pageview');
+  }
 	if($('#home').length){
 		homeMap();
 	}else if($('#tanksIndex').length){
@@ -22,22 +38,6 @@ $(document).ready(function() {
     });
     $('<img/>')[0].src = href;
   });
-  $.cookieBar({
-    message: 'Usamos cookies para poder tener estadísticas de uso de la web. Si continúas navegando estás dando tu consentimiento para la instalación de las cookies.',
-    acceptText: 'Acepto',
-    autoEnable: false,
-    acceptOnContinue: false,
-    acceptOnScroll: true,
-    acceptAnyClick: true
-  });
-  if(jQuery.cookieBar('cookies')){
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-918753-14', 'auto');
-    ga('send', 'pageview');
-  }
 });
 	
 	function homeMap(){
